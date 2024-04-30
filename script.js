6588937f8ec63447cec1166a198137450087c783
Kimberly Maddox <160989452+kmaddox1@users.noreply.github.com>
2024-04-30 22:48:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Also can reassign that variable by just using the variable name to indicate that we're reassigning it.//
+
+
+var myVariable = "Taylor";
+myVariable = "Dr. Small";
+console.log(myVariable);
+
+// Let and Const came into play when we started using ES6.//
+// Let and Const are both block scoped.//
+// Let is a variable that can be reassigned.//
+
+let myDog = "Gia";
+const myZodiac = "Scorpio";
+console.log(myDog);
+console.log(myZodiac);
+
+//Const capitalize the variable name.//
+// Const is a constant variable that cannot be reassigned.//
+
+const STARTINGHP = 500;
+STARTINGHP = 300;
